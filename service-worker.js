741081c01c6a7fdd95ae5873c162390966583bf5
Sayaz06 <adha86614@gmail.com>
2026-01-12 19:53:36 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "stresskedua-cache-v32";
+const CACHE_NAME = "stresskedua-cache-v33";
 const ASSETS = [
   "./",
   "./index.html",
@@ -32,6 +32,7 @@ self.addEventListener("fetch", (event) => {
     caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
+
 
 
 
